b69e956a588c8fe9a9298115945b67c1df9a78e7
Zviad Nemsadze <zviad.nemsadze@magticom.ge>
2017-06-14 19:12:07 +0400

    pom

--- src/main/java/ge/ssoft/chat/init/WebMvcConfig.java
+++ src/main/java/ge/ssoft/chat/init/WebMvcConfig.java
@@ -46,17 +46,17 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 
 
-//    @Override
-//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-//        configurer.favorPathExtension(false).
-//                favorParameter(true).
-//                parameterName("mediaType").
-//                ignoreAcceptHeader(true).
-//                useJaf(false).
-//                defaultContentType(MediaType.APPLICATION_JSON).
-//                mediaType("xml", MediaType.APPLICATION_XML).
-//                mediaType("json", MediaType.APPLICATION_JSON);
-//    }
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false).
+                favorParameter(true).
+                parameterName("mediaType").
+                ignoreAcceptHeader(true).
+                useJaf(false).
+                defaultContentType(MediaType.APPLICATION_JSON).
+                mediaType("xml", MediaType.APPLICATION_XML).
+                mediaType("json", MediaType.APPLICATION_JSON);
+    }
 
 
 
