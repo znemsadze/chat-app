f94c06d60fe6d334c6ef7eb162a296a63098fb41
Zviad Nemsadze <zviad.nemsadze@magticom.ge>
2017-06-15 11:01:07 +0400

    javascript depandencies

--- src/main/webapp/app/controller.js
+++ src/main/webapp/app/controller.js
@@ -2,12 +2,10 @@ angular.module("chatApp.controllers").controller("ChatCtrl", function($scope, Ch
     $scope.messages = [];
     $scope.message = "";
     $scope.max = 140;
-
     $scope.addMessage = function() {
         ChatService.send($scope.message);
         $scope.message = "";
     };
-
     ChatService.receive().then(null, null, function(message) {
         $scope.messages.push(message);
     });
